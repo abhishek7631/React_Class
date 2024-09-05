1e00c7e5ab5016f1a3dfa30385b41478bbdd922d
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-05 21:36:18 +0530

    code modified

--- react-state-class/src/Counter.jsx
+++ react-state-class/src/Counter.jsx
@@ -3,10 +3,14 @@ import { useState } from "react";
 export default function Counter() {
   let [count, setCount] = useState(0);
 
+  let incCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
       <h2>count = {count}</h2>
-      <button>Increase Count</button>
+      <button onClick={incCount}>Increase Count</button>
     </div>
   );
 }
