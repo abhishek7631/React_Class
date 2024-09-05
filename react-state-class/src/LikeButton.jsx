29714420aa4613bd367c39e420adf5a522ee1978
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-05 22:03:57 +0530

    code modified

--- react-state-class/src/LikeButton.jsx
+++ react-state-class/src/LikeButton.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 export default function LikeButton() {
   //   function clicked() {
   //     console.log("clicked");
   //   }
-  let clicked = () => {
-    console.log("clicked");
+
+  let [isLiked, setIsliked] = useState(false);
+  let toggleLike = () => {
+    setIsliked(!isLiked);
   };
+
+  let likeStyle = { color: "red" };
+
   return (
     <div>
-      <p onClick={clicked}>
-        <i className="fa-regular fa-heart"></i>
+      <p onClick={toggleLike}>
+        {isLiked ? (
+          <i className="fa-solid fa-heart" style={likeStyle}></i>
+        ) : (
+          <i className="fa-regular fa-heart"></i>
+        )}
       </p>
     </div>
   );
