d86bbd70234bf179bf80e9e573096b014e2f7fb8
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-06 13:09:57 +0530

    code modified

--- nav-app/src/App.jsx
+++ nav-app/src/App.jsx
@@ -2,9 +2,26 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./Home";
+import Explore from "./Explore";
+import About from "./About";
+import Contact from "./Contact";
+import Navigation from "./Navigation";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <>
+        <Navigation></Navigation>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/explore" element={<Explore></Explore>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+        </Routes>
+      </>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ nav-app/src/Navigation.jsx
@@ -0,0 +1,14 @@
+import "./Navigation.css";
+
+export default function () {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  );
+}
