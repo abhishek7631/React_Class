ffe4c6ba25b48dcf5bb78083e6484bcc208ec1a1
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-11 16:20:17 +0530

    code modified

--- react-state-class2/src/LudoBoard.jsx
+++ react-state-class2/src/LudoBoard.jsx
@@ -9,6 +9,24 @@ export default function LudoBoard() {
       return { ...prevMoves, blue: prevMoves.blue + 1 };
     });
   };
+
+  let updateYellow = () => {
+    setMoves((prevMoves) => {
+      return { ...prevMoves, yellow: prevMoves.yellow + 1 };
+    });
+  };
+
+  let updateGreen = () => {
+    setMoves((prevMoves) => {
+      return { ...prevMoves, green: prevMoves.green + 1 };
+    });
+  };
+
+  let updateRed = () => {
+    setMoves((prevMoves) => {
+      return { ...prevMoves, red: prevMoves.red + 1 };
+    });
+  };
   return (
     <div>
       <p>Game Begins!</p>
@@ -20,15 +38,22 @@ export default function LudoBoard() {
         </button>
 
         <p>Yellow Moves = {moves.yellow} </p>
-        <button style={{ backgroundColor: "yellow", color: "black" }}>
+        <button
+          style={{ backgroundColor: "yellow", color: "black" }}
+          onClick={updateYellow}
+        >
           +1
         </button>
 
         <p>Green Moves = {moves.green} </p>
-        <button style={{ backgroundColor: "green" }}>+1</button>
+        <button style={{ backgroundColor: "green" }} onClick={updateGreen}>
+          +1
+        </button>
 
         <p>Red Moves = {moves.red} </p>
-        <button style={{ backgroundColor: "red" }}>+1</button>
+        <button style={{ backgroundColor: "red" }} onClick={updateRed}>
+          +1
+        </button>
       </div>
     </div>
   );
