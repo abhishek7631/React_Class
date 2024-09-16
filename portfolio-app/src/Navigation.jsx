f3137003cfeb23d8a0420d4c158515559942787d
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-16 13:07:31 +0530

    code modified

--- portfolio-app/src/Navigation.jsx
+++ portfolio-app/src/Navigation.jsx
@@ -5,28 +5,30 @@ export default function Navigation() {
   return (
     <nav>
       <h1>Abhishek,</h1>
-      <ul>
-        <li>
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/internship" className="nav-link">
-            Internship
-          </Link>
-        </li>
-        <li>
-          <Link to="/project" className="nav-link">
-            Project
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="nav-link">
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <div className="menu">
+        <ul>
+          <li>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/internship" className="nav-link">
+              Internship
+            </Link>
+          </li>
+          <li>
+            <Link to="/project" className="nav-link">
+              Project
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="nav-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
