2ad05360171c53f0b6e14cbc15ee3b3d70149f54
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-19 12:34:05 +0530

    code modified

--- ws-class/src/App.jsx
+++ ws-class/src/App.jsx
@@ -1,37 +1,83 @@
 import { useState } from "react";
+import "./App.css";
+import img1 from "./assets/lightOn.jpg";
+import img2 from "./assets/lightOff.png";
 
 function App() {
   const [show, setShow] = useState(false);
-  return (
-    <div className="main">
-      <h4>Choose one Person</h4>
 
+  return (
+    <div className="App">
+      <h2>LIGHT ON/OFF</h2>
       <button
         onClick={() => {
           setShow(true);
         }}
       >
-        Abhishek
+        ON LIGHT
       </button>
       <button
         onClick={() => {
           setShow(false);
         }}
       >
-        Chandan
+        OFF LIGHT
       </button>
 
-      {show ? <Abhishek></Abhishek> : <Chandan></Chandan>}
+      {show ? <On /> : <Off />}
     </div>
   );
 }
 
 export default App;
 
-function Abhishek() {
-  return <p>I'm Abhishek.</p>;
+function On() {
+  return (
+    <div>
+      <img src={img1} alt="on" />
+    </div>
+  );
+}
+function Off() {
+  return (
+    <div>
+      <img src={img2} alt="off" />
+    </div>
+  );
 }
 
-function Chandan() {
-  return <p>I'm Chandan.</p>;
-}
+// function App() {
+//   const [show, setShow] = useState(false);
+//   return (
+//     <div className="main">
+//       <h4>Choose one Person</h4>
+
+//       <button
+//         onClick={() => {
+//           setShow(true);
+//         }}
+//       >
+//         Abhishek
+//       </button>
+//       <button
+//         onClick={() => {
+//           setShow(false);
+//         }}
+//       >
+//         Chandan
+//       </button>
+
+//       {show ? <Abhishek></Abhishek> : <Chandan></Chandan>}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// function Abhishek() {
+//   return <p>I'm Abhishek.</p>;
+// }
+
+// function Chandan() {
+//   return <p>I'm Chandan.</p>;
+// }
