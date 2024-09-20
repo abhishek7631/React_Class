d12621c034f9c0fe71c174d1f83eabd30e955a05
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-20 11:56:47 +0530

    add folder

---
+++ ws-class-02/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  let employe1 = [{ name: "Abhishek", position: "web developer", age: "23" }];
+  let employe2 = [{ name: "Ayush", position: "frontend developer", age: "22" }];
+  let employe3 = [{ name: "Pranav", position: "MERN developer", age: "22" }];
+  let employe4 = [{ name: "Sanket", position: "java developer", age: "21" }];
+  let employe5 = [{ name: "Shrinivas", position: "Aws developer", age: "24" }];
+
+  console.log(employe1);
+  console.log(employe2);
+  console.log(employe3);
+  console.log(employe4);
+  console.log(employe5);
+  return <div></div>;
+}
+
+export default App;
