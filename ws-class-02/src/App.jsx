6623763c50b96c9519267e1dcd45a7d0795c63b5
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-20 13:09:41 +0530

    code modified

--- ws-class-02/src/App.jsx
+++ ws-class-02/src/App.jsx
@@ -5,20 +5,68 @@ function App() {
   return (
     <div>
       <h1>Our Best Employes 2024</h1>
-      <section>
+      <section className="card-container">
         <Card
           name={employe[0].name}
           image={employe[0].img}
           position={employe[0].position}
         ></Card>
+
+        <Card
+          name={employe[1].name}
+          image={employe[1].img}
+          position={employe[1].position}
+        ></Card>
+
+        <Card
+          name={employe[2].name}
+          image={employe[2].img}
+          position={employe[2].position}
+        ></Card>
+
+        <Card
+          name={employe[3].name}
+          image={employe[3].img}
+          position={employe[3].position}
+        ></Card>
+
+        <Card
+          name={employe[4].name}
+          image={employe[4].img}
+          position={employe[4].position}
+        ></Card>
+
+        <Card
+          name={employe[5].name}
+          image={employe[5].img}
+          position={employe[5].position}
+        ></Card>
+
+        <Card
+          name={employe[6].name}
+          image={employe[6].img}
+          position={employe[6].position}
+        ></Card>
+
+        <Card
+          name={employe[7].name}
+          image={employe[7].img}
+          position={employe[7].position}
+        ></Card>
       </section>
+
+      <footer style={{ height: "100px", backgroundColor: "black" }}>
+        <p style={{ color: "white", textAlign: "center", padding: "40px" }}>
+          &copy; 2024 Your Name. All rights reserved.
+        </p>
+      </footer>
     </div>
   );
 }
 
 function Card({ name, image, position }) {
   return (
-    <div>
+    <div className="card">
       <img src={image} alt="image" />
       <h2>{name}</h2>
       <h3>{position}</h3>
--- ws-class-02/src/Data.jsx
+++ ws-class-02/src/Data.jsx
@@ -9,28 +9,52 @@ const employe = [
   {
     id: "2",
     name: "Ayush",
-    img: "https://wallpapercave.com/wp/wp11131009.jpg",
+    img: "https://t4.ftcdn.net/jpg/06/78/09/75/360_F_678097580_mgsNEISedI7fngOwIipYtEU0T6SN8qKv.jpg",
     position: "frontend developer",
     age: "22",
   },
   {
     id: "3",
     name: "Pranav",
-    img: "https://wallpapercave.com/wp/wp11131009.jpg",
+    img: "https://img.freepik.com/free-photo/expressive-redhead-bearded-man-with-hat_176420-32241.jpg",
     position: "MERN developer",
     age: "22",
   },
   {
     id: "4",
     name: "Sanket",
-    img: "https://wallpapercave.com/wp/wp11131009.jpg",
+    img: "https://img.freepik.com/free-photo/funny-young-man-gazes-with-comic-expression-wears-optical-glasses-stylish-shirt-notices-something-interesting-has-pleasant-talk-with-interlocutor-isolated-pink-wall_273609-42340.jpg",
     position: "java developer",
     age: "21",
   },
   {
     id: "5",
     name: "Shrinivas",
-    img: "https://wallpapercave.com/wp/wp11131009.jpg",
+    img: "https://t4.ftcdn.net/jpg/03/25/10/53/360_F_325105363_q4sxFtjwIizvFIrwkGX8zlAEc7bsyqFe.jpg",
+    position: "Aws developer",
+    age: "24",
+  },
+
+  {
+    id: "6",
+    name: "Rahul",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFfLspthhdq9QNlRQY7Pz3JN3n9qx9bVcQ7Wny_gOlBnoS-0IAglCoasAi7aTqHWP7rgA&usqp=CAU",
+    position: "Aws developer",
+    age: "24",
+  },
+
+  {
+    id: "7",
+    name: "Aditya",
+    img: "https://c1.wallpaperflare.com/preview/270/448/927/mentor-gray-blazer-white-shirt-professional.jpg",
+    position: "Aws developer",
+    age: "24",
+  },
+
+  {
+    id: "8",
+    name: "Yogesh",
+    img: "https://c4.wallpaperflare.com/wallpaper/456/1020/998/bushido-rapper-actor-celebrity-wallpaper-preview.jpg",
     position: "Aws developer",
     age: "24",
   },
