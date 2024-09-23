58ed9f1fe778360833f4c50d9c7a6e382c4bba5b
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-23 12:29:02 +0530

    code modified

--- ws-class-03/src/App.jsx
+++ ws-class-03/src/App.jsx
@@ -1,27 +1,20 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(1);
-  const [state, setstate] = useState(1);
+  const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    console.log("hello");
-  });
-
-  function getData() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((item) => {
-        return item.json;
-      })
-      .then((res) => {
-        console.log(res);
-      });
+  async function getData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const result = await res.json();
+    console.log(result);
+    setPosts(result);
   }
 
-  getData();
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
@@ -33,6 +26,22 @@ function App() {
       >
         +
       </button>
+
+      <div>
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            style={{
+              margin: "20px 0",
+              padding: "10px",
+              border: "1px solid #ccc",
+            }}
+          >
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
