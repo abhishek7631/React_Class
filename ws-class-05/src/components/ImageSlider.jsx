b4e3e4f8fed1526aaed9c9ba9569b56ec59c8692
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-26 13:03:03 +0530

    add folder

--- ws-class-05/src/components/ImageSlider.jsx
+++ ws-class-05/src/components/ImageSlider.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import "./ImageSlider.css";
 
 const ImageSlider = () => {
   return (
     <Carousel>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block"
           src="https://img.freepik.com/free-photo/close-up-kitten-by-window_23-2150782379.jpg?size=626&ext=jpg"
           alt="First slide"
         />
@@ -17,7 +18,7 @@ const ImageSlider = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYWJtRwuXyB6FDNp6JrYKSy-GwkEsHax6qdA&s"
           alt="Second slide"
         />
@@ -28,7 +29,7 @@ const ImageSlider = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          className="d-block"
           src="https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
           alt="Third slide"
         />
