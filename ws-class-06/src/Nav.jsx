28b7118a191f26e4f7008031131066694a084473
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-27 12:15:47 +0530

    code modified

---
+++ ws-class-06/src/Nav.jsx
@@ -0,0 +1,16 @@
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h1>Navbar</h1>
+      <div className="menu">
+        <li>Home</li>
+        <li>About</li>
+        <li>Project</li>
+        <li>Services</li>
+        <li>Services</li>
+      </div>
+    </nav>
+  );
+}
