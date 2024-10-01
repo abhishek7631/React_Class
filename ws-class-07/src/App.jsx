9a3ae05a7fccc3b62ec9de603293450507555592
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-01 07:57:35 +0530

    code modified

--- ws-class-07/src/App.jsx
+++ ws-class-07/src/App.jsx
@@ -1,37 +1,69 @@
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
-  const [middlename, setMiddlename] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [age, setAge] = useState("");
-  const [email, setEmail] = useState("");
-  const [adharnum, setAdharnum] = useState("");
+  const [formData, setFormData] = useState({
+    firstname: "",
+    middlename: "",
+    lastname: "",
+    age: "",
+    email: "",
+    adharnum: "",
+  });
 
-  const data = (e) => {
-    e.preventDefault();
-    console.log(
-      `Name: ${name}, mName: ${middlename}, lName: ${lastname}, Age: ${age}, Email: ${email}, aNumber: ${adharnum}`
-    );
+  const handleInputChange = (event) => {
+    let fieldName = event.target.name;
+    let newValue = event.target.value;
+
+    setFormData((currData) => {
+      currData[fieldName] = newValue;
+      return { ...currData };
+    });
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    setFormData({
+      firstname: "",
+      middlename: "",
+      lastname: "",
+      age: "",
+      email: "",
+      adharnum: "",
+    });
+  };
+
+  // const [name, setName] = useState("");
+  // const [middlename, setMiddlename] = useState("");
+  // const [lastname, setLastname] = useState("");
+  // const [age, setAge] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [adharnum, setAdharnum] = useState("");
+
+  // const data = (e) => {
+  //   e.preventDefault();
+  //   console.log(
+  //     `Name: ${name}, mName: ${middlename}, lName: ${lastname}, Age: ${age}, Email: ${email}, aNumber: ${adharnum}`
+  //   );
+  // };
 
   return (
     <div className="bg-info py-4">
       <h1 className="text-center my-2">Student Form</h1>
       <hr />
       <form
-        onSubmit={data}
         className="d-flex flex-column w-50 mx-auto border border-3 py-4 px-5"
+        onSubmit={handleSubmit}
       >
-        <label htmlFor="name">Your First Name:</label>
+        <label htmlFor="fName">Your First Name:</label>
         <input
           className="mb-2"
           type="text"
-          id="name"
+          id="fName"
           placeholder="Enter Your Name"
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
+          value={formData.firstname}
+          name="firstname"
+          onChange={handleInputChange}
         />
 
         <label htmlFor="mName">Your Middle Name:</label>
@@ -40,9 +72,9 @@ function App() {
           type="text"
           id="mName"
           placeholder="Enter Your Middle Name"
-          onChange={(e) => {
-            setMiddlename(e.target.value);
-          }}
+          value={formData.middlename}
+          name="middlename"
+          onChange={handleInputChange}
         />
 
         <label htmlFor="lName">Your Last Name:</label>
@@ -51,9 +83,9 @@ function App() {
           type="text"
           id="lName"
           placeholder="Enter Your Last Name"
-          onChange={(e) => {
-            setLastname(e.target.value);
-          }}
+          value={formData.lastname}
+          name="lastname"
+          onChange={handleInputChange}
         />
 
         <label htmlFor="age">Your Age:</label>
@@ -62,9 +94,9 @@ function App() {
           type="text"
           id="age"
           placeholder="Enter Your age"
-          onChange={(e) => {
-            setAge(e.target.value);
-          }}
+          value={formData.age}
+          name="age"
+          onChange={handleInputChange}
         />
         <label htmlFor="email">Your Email:</label>
         <input
@@ -72,9 +104,9 @@ function App() {
           type="text"
           id="email"
           placeholder="Enter Your Email"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
+          value={formData.email}
+          name="email"
+          onChange={handleInputChange}
         />
 
         <label htmlFor="Anumber">Your Adhar Number:</label>
@@ -83,9 +115,9 @@ function App() {
           type="text"
           id="Anumber"
           placeholder="Enter Your Adhar Number"
-          onChange={(e) => {
-            setAdharnum(e.target.value);
-          }}
+          value={formData.adharnum}
+          name="adharnum"
+          onChange={handleInputChange}
         />
 
         <input
