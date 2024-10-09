70504e7f5467faff018c224b24aa8876bd31c2a0
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-09 12:38:08 +0530

    code modified

--- blog_app_CRUD/src/App.jsx
+++ blog_app_CRUD/src/App.jsx
@@ -1,23 +1,32 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-// import BlogList from "./Components/BlogList";
+import "./App.css";
 import BlogList from "./components/BlogList";
-import BlogForm from "./Components/BlogForm";
-import EditBlog from "./Components/EditBlog";
+import BlogForm from "./components/BlogForm";
+import EditBlog from "./components/EditBlog";
+// import BlogList from "./Components/BlogList";
+// import BlogList from "./components/BlogList";
+// import BlogForm from "./Components/BlogForm";
+// import EditBlog from "./Components/EditBlog";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/create" style={{ marginLeft: "10px" }}>
-            Create Blog
-          </Link>
+      <div>
+        <nav className="nav">
+          <h1>MyBlog</h1>
+          <div className="menu">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+
+            <Link to="/create">
+              <li>Create Blog</li>
+            </Link>
+          </div>
         </nav>
-        <Routes>
-          <Route path="/" element={<BlogList />} />
-          <Route path="/create" element={<BlogForm />} />
-          <Route path="/edit/:id" element={<EditBlog />} />
+        <Routes path="/" element={<BlogList></BlogList>}>
+          <Route path="/Create" element={<BlogForm></BlogForm>}></Route>
+          <Route path="/edit/:id" element={<EditBlog></EditBlog>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
