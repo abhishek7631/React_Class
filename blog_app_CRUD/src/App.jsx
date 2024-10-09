90775e457e8d0cb766192b3c3b2076a7696cdece
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-09 12:04:17 +0530

    code modified

--- blog_app_CRUD/src/App.jsx
+++ blog_app_CRUD/src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+// import BlogList from "./Components/BlogList";
+import BlogList from "./components/BlogList";
+import BlogForm from "./Components/BlogForm";
+import EditBlog from "./Components/EditBlog";
+
 function App() {
   return (
-    <div>
-      <h1>This is our app</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/create" style={{ marginLeft: "10px" }}>
+            Create Blog
+          </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<BlogList />} />
+          <Route path="/create" element={<BlogForm />} />
+          <Route path="/edit/:id" element={<EditBlog />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
