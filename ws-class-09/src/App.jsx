4b7badf4bd90886d4b41121a4b6c379c7a98cb5f
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-18 12:01:17 +0530

    add folder

--- ws-class-09/src/App.jsx
+++ ws-class-09/src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setData(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <h1>heloo</h1>
+      {data.slice(0, 10).map((items) => {
+        const { title, id } = items;
+        return (
+          <h4 key={id}>
+            {id}.{title}
+          </h4>
+        );
+      })}
+
+      <section
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <button>Prev</button>
+        <button>Next</button>
+      </section>
     </div>
   );
 }
